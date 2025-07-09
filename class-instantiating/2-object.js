5a8d1072656ca6ac653401d071db0c968d5c83d9
mahdiaslami <mahdyaslami@gmail.com>
2025-07-09 13:02:19 +0330

    feat: add new test case for class creation
    In this commit a new test case for class creation added and 2-simple
    test remained for a simple iteration

---
+++ class-instantiating/2-object.js
@@ -0,0 +1,5 @@
+console.time('time');
+for (let i = 0; i < 1e7; i++) {
+    ({ x: 1, y: 2 });
+}
+console.timeEnd('time');
--- class-instantiating/2-simple.js
+++ class-instantiating/2-simple.js
@@ -1,5 +1,3 @@
 console.time('time');
-for (let i = 0; i < 1e7; i++) {
-    // ({ x: 1, y: 2 });
-}
+for (let i = 0; i < 1e7; i++);
 console.timeEnd('time');
